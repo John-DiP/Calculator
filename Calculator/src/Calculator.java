0387fe23087209159fa34dce5ce7e3074397db90
John D. <103323388+John-DiP@users.noreply.github.com>
2025-01-03 22:30:58 -0600

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static int add(int num1, int num2) {
+		
+		int result = num1 + num2;
+		
+		return result;
+	}
+	
+	public static int sub(int num1, int num2) {
+		
+		int result = num1 - num2;
+		
+		return result;
+	}
+	
+	public static int multiply(int num1, int num2) {
+		
+		int result = num1 * num2;
+		
+		return result;
+	}
+	
+	public static int division(int num1, int num2) {
+		
+		int result = num1 / num2;
+		
+		return result;
+	}
+	
+	public static int mod(int num1, int num2) {
+		
+		int result = num1 % num2;
+		
+		return result;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner userInput = new Scanner(System.in);
+			
+		String calcContinue = "continue";
+		
+		while (!calcContinue.equalsIgnoreCase("!")) {
+			
+			System.out.println("Enter the first number");
+			int num1 = userInput.nextInt();
+		
+			userInput.nextLine();
+		
+			System.out.println("Enter a operator(+, -, *, /, mod)");
+			String sign = userInput.nextLine();
+		
+			System.out.println("Enter the second number");
+			int num2 = userInput.nextInt();
+		
+			if (sign.equals("+")) {
+				System.out.println("result: " + add(num1,num2));
+			
+			} else if (sign.equals("-")) {
+				System.out.println("result: " + sub(num1,num2));
+			
+			} else if (sign.equals("*")) {
+				System.out.println("result: " + multiply(num1,num2));
+			
+			} else if (sign.equals("/")) {
+			
+				if (num2 == 0) {
+					System.out.println("Error: cannot divide by 0");
+				} else {
+					System.out.println("result: " + division(num1,num2));
+				}
+			
+			} else if (sign.equals("mod")) {
+				System.out.println("result: " + mod(num1,num2));
+		
+			} else {
+				System.out.println("invalid operator");
+			}
+		
+			System.out.println("Would you like to go again? Press ! to exit");
+			calcContinue = userInput.nextLine();
+			
+			userInput.nextLine();
+		}	
+		
+		
+		userInput.close();
+	}
+		
+}
